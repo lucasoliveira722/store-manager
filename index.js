39e8bd66302d6734bb20b317f67382fcf4b637fc
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-04-01 21:58:15 -0300

    Início da configuração da camada Model

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
