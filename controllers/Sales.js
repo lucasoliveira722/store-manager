94b4303be4331602d804af86ffe6963271f0eee1
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-04-04 14:58:30 -0300

    Correção de erros de lint

--- controllers/Sales.js
+++ controllers/Sales.js
@@ -6,7 +6,7 @@ const getAll = async (req, res) => {
   res.status(200).json(sales);
 };
 
-const getById = async (req, res, next) => {
+const getById = async (req, res) => {
   const { id } = req.params;
 
   const sale = await Sales.getById(id);
