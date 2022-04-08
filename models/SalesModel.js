2af83a0fe1a6b7d9a9e4e23ab6e59bbc482c3d9c
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-04-08 13:30:34 -0300

    Correções

--- models/SalesModel.js
+++ models/SalesModel.js
@@ -61,7 +61,7 @@ const update = async (id, products) => {
   products.forEach(async ({ productId, quantity }) => {
     await connection.execute(
       `UPDATE StoreManager.sales_products
-      SET quantity = ? WHERE sale_id = ? AND product_id = ?`,
+      SET product_id = ?, quantity = ? WHERE sale_id = ?`,
       [productId, quantity, id],
     );
   });
